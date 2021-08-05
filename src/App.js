660d21083d1807f20fe976e2cf779ad7feb6b862
Can Guven <bcanguven@gmail.com>
2021-08-05 17:28:50 +0100

    Post Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar'
+import Post from './components/post/post'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Post />
 
     </>
   );
